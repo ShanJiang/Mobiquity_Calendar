edacc957699d2c015eb6b088bae85c923074ae0e
Shan Jiang <shanjiang007@gmail.com>
2015-01-02 02:38:15 -0500

    text align

---
+++ EventCalendar/Models/IdentityModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EventCalendar.Models
+{
+    
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
